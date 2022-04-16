91f250895a6663ab9191cd8b999e945cacfcf817
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-04-17 03:05:04 +0600

    new added

--- src/Pages/DetailService/DetailService.js
+++ src/Pages/DetailService/DetailService.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const DetailService = () => {
-    const {detailId}= useParams();
-    return (
-        <div>
-          <h1>Welcome to Details: {detailId}</h1>  
-        </div>
-    );
+  const { detailId } = useParams();
+  return (
+    <div>
+      <h1>Welcome to Details: {detailId}</h1>
+      <div className='text-center'>
+        <Link to="/checkout">
+          <button className='btn btn-danger'>Proceed to Checkout</button>
+        </Link>
+      </div>
+
+    </div>
+  );
 };
 
 export default DetailService;
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,23 +1,40 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
-const handleSubmit = event => {
-    event.preventDefault();
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
 
 
-    console.log(email, password);
-}
+        signInWithEmailAndPassword(email, password);
+    }
 
-const navigateRegister = event => {
-navigate("/register")
-}
+    const navigateRegister = event => {
+        navigate("/register")
+    }
 
     return (
         <div className='container w-50 mx-auto '>
@@ -25,7 +42,7 @@ navigate("/register")
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -33,13 +50,13 @@ navigate("/register")
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <Button variant="danger" type="submit">
-                   Login
+                    Login
                 </Button>
             </Form>
             <p>New to The Car Doctor? <Link to={"/register"} className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
